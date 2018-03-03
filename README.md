7cbd8bb6a13227d806516ba764bd894f29b6cecf
tonkytonky <as@asamsonov.me>
2018-03-04 00:02:14 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # Github Trends
 
-[TODO. There will be project description]
+This is the script for dumping information about recently created GitHub 
+repositories. It returns defined number of the most popular repositories 
+sorted by the number of stars and the number of open issues for which of them.
+
+
+# Usage
+
+To run the script you need Python 3.5 or higher to be installed. 
+
+Example:
+```bash
+$ python github_trendinf.py [-n NUMBER] [-d DAYS]
+issues: <number of open issues>, link: <link to repository>
+```
+`-n NUMBER` - number of repositories to get  
+`-d DAYS` - number of days for which to get repositories
+
 
 # Project Goals
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+The code is written for educational purposes. 
+Training course for web-developers - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,68 @@
+import argparse
+from datetime import datetime, timedelta
+import requests
+from urllib.parse import urljoin
 
 
-def get_trending_repositories(top_size):
-    pass
+def _main():
+    args = get_args(argparse.ArgumentParser())
+    trending_repositories = get_trending_repositories(args.number, args.days)
+    for trending_repository in trending_repositories:
+        print('issues: {}, link: {}'.format(
+            trending_repository['issues'],
+            trending_repository['link'])
+        )
+
+
+def get_args(parser):
+    parser.add_argument(
+        '-n',
+        '--number',
+        help='Number of repositories to get',
+        type=int,
+        default=20
+    )
+    parser.add_argument(
+        '-d',
+        '--days',
+        help='Number of days for which to get repositories',
+        type=int,
+        default=7
+    )
+
+    return parser.parse_args()
+
+
+def get_trending_repositories(top_size, days):
+    start_date = datetime.now() - timedelta(days=days)
+    repos = requests.get(
+        url='https://api.github.com/search/repositories',
+        params={
+            'q': 'created:>2018-02-15'.format(start_date.strftime('%Y-%m-%d')),
+            'sort': 'stars',
+            'order': 'desc',
+        }
+    ).json()
+
+    for repo in repos['items'][:top_size]:
+        open_issues_amount = get_open_issues_amount(
+            repo['owner']['login'],
+            repo['name']
+        )
+        link = repo['html_url']
+        yield {'issues': open_issues_amount, 'link': link}
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    url = urljoin(
+        'https://api.github.com',
+        'repos/{repo_owner}/{repo_name}/issues'.format(
+            repo_owner=repo_owner,
+            repo_name=repo_name,
+        )
+    )
+    return len(requests.get(url).json())
+
 
 if __name__ == '__main__':
-    pass
+    _main()
